83051713471da67bab39e60a4e44317953f71275
Saish Sankhe <saishsankhe23@gmail.com>
2021-04-03 01:37:25 -0400

    Update loading state, added lazy loading

--- src/components/ShowPhotos.vue
+++ src/components/ShowPhotos.vue
@@ -1,18 +1,19 @@
 <template> 
     <div class="photos-grid">
-        <!-- Fetching photos condition -->
-        <h2 v-if="!info" class="text-center font-weight-light blue--text text--darken-4">Loading...</h2>
-        
         <!-- No photos condition -->
         <h2 v-if="info.length==0" class="text-center font-weight-light blue--text text--darken-4">No photos available for selected filters.</h2>
         
         <!-- if photos found -->
-        <ul v-else>
-        <!-- loop through the array of data -->
-        <li v-for="item in info" :key="item.id">
-            <img :src = "item.img_src" width=200px height="auto"/>
-        </li>
+        <ul v-else-if="info.length!=0">
+            <!-- loop through the array of data -->
+            <li v-for="item in info" :key="item.id">
+                <img :src = "item.img_src" width=200px height="auto" loading="lazy/>
+            </li>
         </ul>
+        
+        
+        <!-- Fetching photos condition -->
+        <h2 v-else class="text-center font-weight-light blue--text text--darken-4">Loading...</h2>
     </div>
 </template>
 
